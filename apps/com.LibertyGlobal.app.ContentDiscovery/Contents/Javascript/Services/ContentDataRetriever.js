df21ebdf09011fb93ac486f6f15bd794871f5477
Ralph Soons <rsoons@lgi.com>
2014-10-28 13:18:28 +0100

    fix warnings

--- apps/com.LibertyGlobal.app.ContentDiscovery/Contents/Javascript/Services/ContentDataRetriever.js
+++ apps/com.LibertyGlobal.app.ContentDiscovery/Contents/Javascript/Services/ContentDataRetriever.js
@@ -2,20 +2,6 @@ function ContentDataRetriever() {
 	var callbackAfterLoaded;
 	var callbackAfterLoadedParams;
 
-	this.loadMenuDataExtended = function(menuItem, callback, callbackParams){
-		LGI.Guide.Broadcast.create()
-		.limit(10)
-		.fields(LGI.Guide.Video.ID, LGI.Guide.Broadcast.TITLE, LGI.Guide.Broadcast.START, 
-			LGI.Guide.Broadcast.END, LGI.Guide.Broadcast.CHANNEL,
-			LGI.Guide.Broadcast.SYNOPSIS, LGI.Guide.Broadcast.IMAGE_LINK, LGI.Guide.Broadcast.CATEGORY)
-		.filter(LGI.Guide.Broadcast.START.greaterThan(currentTime))					
-		.filter(LGI.Guide.Broadcast.CATEGORY.equalTo(menuItem.categoryFilters))
-		.sort(LGI.Guide.Broadcast.START)
-		.findOne(function(response){ 
-			parseData(menuItem, response, futureAssets);
-		});
-	};
-
 	this.loadMenuData = function(menuItem, callback, callbackParams){
 		callbackAfterLoaded = callback;
 		callbackAfterLoadedParams = callbackParams;	
